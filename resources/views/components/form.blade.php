33e9018857bf8e8160f6881c38c8eddcde8dee37
Erik Bernskiold <erik@bernskioldmedia.com>
2024-04-05 21:09:40 +0200

    Only load form if available

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,7 +1,9 @@
 @props(['form'])
-<x-dynamic-component :component="config('form-architect.components.form')"
-                     :attributes="$form->toViewComponentAttributes()">
-    @foreach($form->fields as $field)
-        <x-form-architect::form-field :field="$field"/>
-    @endforeach
-</x-dynamic-component>
+@if($form)
+    <x-dynamic-component :component="config('form-architect.components.form')"
+                         :attributes="$form->toViewComponentAttributes()">
+        @foreach($form->fields as $field)
+            <x-form-architect::form-field :field="$field"/>
+        @endforeach
+    </x-dynamic-component>
+@endif
