319516328c55e6839e0db9fd6229cd0fcefdedf7
Erik Bernskiold <erik@bernskioldmedia.com>
2024-04-05 16:57:35 +0200

    Load config

--- src/LaravelFormArchitectServiceProvider.php
+++ src/LaravelFormArchitectServiceProvider.php
@@ -4,6 +4,7 @@ namespace BernskioldMedia\LaravelFormArchitect;
 
 use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
+use function config_path;
 
 class LaravelFormArchitectServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,17 @@ class LaravelFormArchitectServiceProvider extends ServiceProvider
         AboutCommand::add('Laravel Form Architect', fn() => [
             'Version' => '1.0.0',
         ]);
+
+        $this->publishes([
+            __DIR__ . '/.../config/form-architect.php' => config_path('form-architect.php'),
+        ], 'laravel-form-architect-config');
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/.../config/form-architect.php', 'form-architect'
+        );
     }
 
 }
